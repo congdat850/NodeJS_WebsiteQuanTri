c1176694572e18c790143cb6bf17998703d08ea8
congdat850 <congdat.1088@gmail.com>
2019-04-22 19:12:35 +0700

    add all

---
+++ routes/index.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('Top10Product', { title: 'Express' });
+});
+router.get('/Top10SanPhamBanChay', function(req, res, next) {
+  res.render('Top10Product', { title: 'Express' });
+});
+//User
+router.get('/TatCaNguoiDung', function(req, res, next) {
+  res.render('UserManagement', { title: 'Express' });
+});
+
+router.get('/ThemNguoiDung', function(req, res, next) {
+  res.render('AddUser', { title: 'Express' });
+});
+
+router.get('/ChinhSuaNguoiDung', function(req, res, next) {
+  res.render('EditUser', { title: 'Express' });
+});
+
+router.get('/ThongKeDoanhSo', function(req, res, next) {
+  res.render('SalesStatistics', { title: 'Express' });
+});
+// Product
+router.get('/DanhSachSanPham', function(req, res, next) {
+  res.render('ProductManagement', { title: 'Express' });
+});
+router.get('/ThemSanPham', function(req, res, next) {
+  res.render('AddProduct', { title: 'Express' });
+});
+router.get('/ChinhSuaSanPham', function(req, res, next) {
+  res.render('EditProduct', { title: 'Express' });
+});
+// Order Management
+router.get('/DanhSachDonDatHang', function(req, res, next) {
+  res.render('OrderManagement', { title: 'Express' });
+});
+router.get('/ThemDonDatHang', function(req, res, next) {
+  res.render('AddOrder', { title: 'Express' });
+});
+router.get('/ChinhSuaDonDatHang', function(req, res, next) {
+  res.render('EditOrder', { title: 'Express' });
+});
+
+module.exports = router;
